9f013a645e1edd15da688b5aa7e58e646b345988
s3777091 <s3777091@rmit.edu.vn>
2024-08-18 08:12:56 +0700

    change

--- data.h
+++ data.h
@@ -10,6 +10,11 @@ const char index_html[] PROGMEM = R"rawliteral(
       content="width=device-width, initial-scale=1.0, user-scalable=no"
     />
     <style>
+      html,
+      body {
+        overflow: hidden;
+        height: 100%;
+      }
       body {
         font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS",
           sans-serif;
@@ -17,32 +22,57 @@ const char index_html[] PROGMEM = R"rawliteral(
         font-size: 1.5rem;
         margin: 0;
         width: 100%;
-        height: 100%;
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        position: fixed;
       }
-
-      h1 {
-        text-align: center;
-        margin: 0.5rem;
-      }
-
+      h1,
       p {
         text-align: center;
         margin: 0.5rem;
       }
-
       #canvas {
         display: block;
         border: 1px solid #ddd;
-        touch-action: none; /* Prevent scrolling on touch devices while interacting with the canvas */
+        touch-action: none;
       }
-
-      #slider {
+      .slider {
+        -webkit-appearance: none;
         width: 80%;
+        height: 10px;
+        border-radius: 5px;
+        background-color: #4158d0;
+        background-image: linear-gradient(
+          43deg,
+          #4158d0 0%,
+          #c850c0 46%,
+          #ffcc70 100%
+        );
+        outline: none;
+        opacity: 0.7;
+        -webkit-transition: 0.2s;
+        transition: opacity 0.2s;
         margin: 1rem;
+      }
+      .slider::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        appearance: none;
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        background-color: #4c00ff;
+        background-image: linear-gradient(160deg, #4900f5 0%, #80d0c7 100%);
+        cursor: pointer;
+      }
+      .slider::-moz-range-thumb {
+        width: 20px;
+        height: 20px;
+        border-radius: 50%;
+        background-color: #0093e9;
+        background-image: linear-gradient(160deg, #0093e9 0%, #80d0c7 100%);
+        cursor: pointer;
       }
     </style>
   </head>
@@ -56,63 +86,49 @@ const char index_html[] PROGMEM = R"rawliteral(
       θ1: <span id="theta1">0</span> θ2: <span id="theta2">0</span> θ3:
       <span id="theta3">0</span>
     </p>
-
-    <input type="range" id="slider" min="0" max="100" value="50" />
+    <input
+      type="range"
+      id="slider"
+      class="slider"
+      min="0"
+      max="100"
+      value="50"
+    />
     <canvas id="canvas"></canvas>
 
     <script type="text/javascript">
-      var canvas, ctx;
-      var x_orig, y_orig, outerRadius, innerRadius;
-      var paint = false;
-      var coord = { x: 0, y: 0 };
-      var websocket;
-
-      const minValues = { x: -124.02, y: -134.32, z: -323.24 };
-      const maxValues = { x: 124.02, y: 139.53, z: -173.49 };
+      var canvas,
+        ctx,
+        paint = false,
+        coord = { x: 0, y: 0 };
+      const minValues = { x: -124, y: -134, z: -322 };
+      const maxValues = { x: 124, y: 139, z: -173 };
+      var x_orig, y_orig, outerRadius;
 
       async function init() {
-        // Get WebSocket URL from window location href
         var url = "ws://" + window.location.hostname + ":1337/";
-        console.log("Connecting to WebSocket at " + url);
-
-        // Connect to WebSocket server
         await wsConnect(url);
       }
 
       async function wsConnect(url) {
-        // Connect to WebSocket server
         websocket = new WebSocket(url);
-
-        // Assign callbacks
         websocket.onopen = function (evt) {
-          onOpen(evt);
+          console.log("Connected to WebSocket");
         };
         websocket.onclose = function (evt) {
-          onClose(evt);
+          setTimeout(function () {
+            wsConnect(websocket.url);
+          }, 2000);
         };
         websocket.onmessage = function (evt) {
           onMessage(evt);
         };
         websocket.onerror = function (evt) {
-          onError(evt);
+          console.log("WebSocket error: " + evt.data);
         };
       }
 
-      function onOpen(evt) {
-        // Log connection state
-        console.log("Connected to WebSocket");
-      }
-
-      function onClose(evt) {
-        // Try to reconnect after a few seconds
-        setTimeout(function () {
-          wsConnect(websocket.url);
-        }, 2000);
-      }
-
       function onMessage(evt) {
-        // Print out our received message
-        console.log("Received: " + evt.data);
         let data = JSON.parse(evt.data);
         document.getElementById("x_coordinate").innerText = data.x;
         document.getElementById("y_coordinate").innerText = data.y;
@@ -120,15 +136,6 @@ const char index_html[] PROGMEM = R"rawliteral(
         document.getElementById("theta1").innerText = data.theta1.toFixed(2);
         document.getElementById("theta2").innerText = data.theta2.toFixed(2);
         document.getElementById("theta3").innerText = data.theta3.toFixed(2);
-        coord.x = data.coord_x;
-        coord.y = data.coord_y;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        background();
-        joystick(data.coord_x, data.coord_y);
-      }
-
-      function onError(evt) {
-        console.log("WebSocket error: " + evt.data);
       }
 
       function doSend(message) {
@@ -139,54 +146,48 @@ const char index_html[] PROGMEM = R"rawliteral(
         init();
         canvas = document.getElementById("canvas");
         ctx = canvas.getContext("2d");
-        resize();
+        resizeCanvas();
+
         document.addEventListener("mousedown", startDrawing);
         document.addEventListener("mouseup", stopDrawing);
         document.addEventListener("mousemove", draw);
         document.addEventListener("touchstart", startDrawing);
         document.addEventListener("touchend", stopDrawing);
-        document.addEventListener("touchcancel", stopDrawing);
         document.addEventListener("touchmove", draw);
-        window.addEventListener("resize", resize);
+        window.addEventListener("resize", resizeCanvas);
+
+        // Add event listener for the slider
 
         var slider = document.getElementById("slider");
         slider.addEventListener("input", updateSlider);
       });
 
-      function resize() {
+      function resizeCanvas() {
         var minDimension = Math.min(window.innerWidth, window.innerHeight);
         outerRadius = minDimension / 4;
-        innerRadius = outerRadius / 2;
         canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight * 0.8; // Increased height to center properly
-        background();
-        joystick(canvas.width / 2, canvas.height / 2); // Centered joystick
+        canvas.height = window.innerHeight * 0.8;
+        drawBackground();
+        drawJoystick(canvas.width / 2, canvas.height / 2);
       }
 
-      function background() {
+      function drawBackground() {
         x_orig = canvas.width / 2;
-        y_orig = canvas.height / 2; // Centered joystick
+        y_orig = canvas.height / 2;
         ctx.beginPath();
-        ctx.arc(x_orig, y_orig, outerRadius - 10, 0, Math.PI * 2, true);
-        ctx.fillStyle = "#ECE5E5";
+        ctx.arc(x_orig, y_orig, outerRadius, 0, Math.PI * 2, true);
+        ctx.fillStyle = "#BED7DC";
         ctx.fill();
       }
 
-      function joystick(x, y) {
-        // Outer circle
+      function drawJoystick(x, y) {
         ctx.beginPath();
-        ctx.arc(x, y, outerRadius - 50, 0, Math.PI * 2, true);
-        ctx.fillStyle = "#F08080";
+        ctx.arc(x, y, outerRadius - 30, 0, Math.PI * 2, true);
+        ctx.fillStyle = "#000000";
         ctx.fill();
-        ctx.strokeStyle = "#F6ABAB";
-        ctx.lineWidth = 8;
+        ctx.strokeStyle = "#E5DDC5";
+        ctx.lineWidth = 10;
         ctx.stroke();
-
-        // Inner circle
-        ctx.beginPath();
-        ctx.arc(x, y, innerRadius - 20, 0, Math.PI * 2, true);
-        ctx.fillStyle = "#333";
-        ctx.fill();
       }
 
       function getPosition(event) {
@@ -196,7 +197,7 @@ const char index_html[] PROGMEM = R"rawliteral(
         coord.y = mouse_y - canvas.offsetTop;
       }
 
-      function isItInTheCircle() {
+      function isInCircle() {
         var current_radius = Math.sqrt(
           Math.pow(coord.x - x_orig, 2) + Math.pow(coord.y - y_orig, 2)
         );
@@ -204,39 +205,35 @@ const char index_html[] PROGMEM = R"rawliteral(
       }
 
       function startDrawing(event) {
-        paint = true;
         getPosition(event);
-        if (isItInTheCircle()) {
+        if (isInCircle()) {
+          paint = true;
           ctx.clearRect(0, 0, canvas.width, canvas.height);
-          background();
-          joystick(coord.x, coord.y);
-          draw();
+          drawBackground();
+          drawJoystick(coord.x, coord.y);
+          draw(event);
         }
       }
 
       async function draw(event) {
         if (paint) {
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          background();
           getPosition(event);
           var angle = Math.atan2(coord.y - y_orig, coord.x - x_orig);
-          var angle_in_degrees =
-            Math.sign(angle) === -1
-              ? Math.round((-angle * 180) / Math.PI)
-              : Math.round(360 - (angle * 180) / Math.PI);
-          if (isItInTheCircle()) {
-            joystick(coord.x, coord.y);
-            x = coord.x;
-            y = coord.y;
+          if (isInCircle()) {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            drawBackground();
+            drawJoystick(coord.x, coord.y);
           } else {
-            x = outerRadius * Math.cos(angle) + x_orig;
-            y = outerRadius * Math.sin(angle) + y_orig;
-            joystick(x, y);
+            coord.x = outerRadius * Math.cos(angle) + x_orig;
+            coord.y = outerRadius * Math.sin(angle) + y_orig;
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            drawBackground();
+            drawJoystick(coord.x, coord.y);
           }
 
           var x_relative = Math.round(
             mapValue(
-              x - x_orig,
+              coord.x - x_orig,
               -outerRadius,
               outerRadius,
               minValues.x,
@@ -245,15 +242,18 @@ const char index_html[] PROGMEM = R"rawliteral(
           );
           var y_relative = Math.round(
             mapValue(
-              y - y_orig,
+              coord.y - y_orig,
               -outerRadius,
               outerRadius,
               minValues.y,
               maxValues.y
             )
           );
+
+          // Update the display with the calculated values
           document.getElementById("x_coordinate").innerText = x_relative;
           document.getElementById("y_coordinate").innerText = y_relative;
+
           await send(x_relative, y_relative);
         }
       }
@@ -269,24 +269,20 @@ const char index_html[] PROGMEM = R"rawliteral(
       async function send(x, y) {
         var zValue = document.getElementById("slider").value;
         var z = Math.round(mapValue(zValue, 0, 100, minValues.z, maxValues.z));
-        document.getElementById("z_value").innerText = z;
-
         var { theta1, theta2, theta3, fl } = IKinem(x, y, z);
+
         if (fl === 0) {
           document.getElementById("theta1").innerText = theta1.toFixed(2);
           document.getElementById("theta2").innerText = theta2.toFixed(2);
           document.getElementById("theta3").innerText = theta3.toFixed(2);
 
-          // Send data to WebSocket server
           doSend({
-            x: x,
-            y: y,
-            z: z,
-            theta1: theta1,
-            theta2: theta2,
-            theta3: theta3,
-            coord_x: coord.x,
-            coord_y: coord.y,
+              x: x,
+              y: y,
+              z: z,
+              theta1: theta1,
+              theta2: theta2,
+              theta3: theta3,
           });
         } else {
           document.getElementById("theta1").innerText = "N/A";
@@ -313,7 +309,7 @@ const char index_html[] PROGMEM = R"rawliteral(
 
         function IKinemTh(x0, y0, z0) {
           const y1 = -R;
-          y0 = y0 - r; // shift center to edge
+          y0 = y0 - r;
 
           const a =
             (x0 * x0 + y0 * y0 + z0 * z0 + L * L - l * l - y1 * y1) / (2 * z0);
@@ -321,7 +317,7 @@ const char index_html[] PROGMEM = R"rawliteral(
           const D = -(a + b * y1) * (a + b * y1) + L * L * (b * b + 1);
 
           if (D < 0) {
-            return NaN; // non-existing
+            return NaN;
           } else {
             const yj = (y1 - a * b - Math.sqrt(D)) / (b * b + 1);
             const zj = a + b * yj;
@@ -329,8 +325,7 @@ const char index_html[] PROGMEM = R"rawliteral(
             if (yj > y1) {
               theta += Math.PI;
             }
-            theta = (theta * 180) / Math.PI; // Convert to degrees
-            return theta;
+            return (theta * 180) / Math.PI;
           }
         }
 
